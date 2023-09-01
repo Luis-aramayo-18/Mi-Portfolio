3806619e5b4ced5e4074915e6679f9da2c7bcf06
Luis Aramayo <luis.aramayo.18@gmail.com>
2023-09-01 18:26:19 -0300

    termino seccion de about

--- src/script.js
+++ src/script.js
@@ -5,3 +5,28 @@ function toggleMenu(){
     menu.classList.toggle('open')
     icon.classList.toggle('open')
 }
+
+function descargarCV() {
+    const url = '../assets/curriculumVitaeMe.pdf';
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', 'AramayoLuisCV.pdf');
+    link.click();
+  }
+
+function openTab(tabName) {
+    let i, tabContent, tabLinks;
+    
+    tabContent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabContent.length; i++) {
+      tabContent[i].style.display = "none";
+    }
+    
+    tabLinks = document.getElementsByClassName("tab-links");
+    for (i = 0; i < tabLinks.length; i++) {
+      tabLinks[i].classList.remove("active-link");
+    }
+    
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.classList.add("active-link");
+  }
