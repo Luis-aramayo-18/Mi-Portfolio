4cfbc7fb3900ae7d5ec3b5e6d19dadad6826f860
Luis Aramayo <luis.aramayo.18@gmail.com>
2023-09-09 21:41:19 -0300

    termino el portfolio, aun falta corregir estilos

--- src/script.js
+++ src/script.js
@@ -136,26 +136,82 @@ function openTab(tabName) {
     ringPorcentage.textContent = ring.progreso + '%';
   });
 
-  function openModal(){
-    const body = document.body
-    const navBar = document.querySelector('#desktop-nav');
-    const cardServices = document.querySelector('.card');
-    const modal = document.querySelector('.modal');
+  const servicesModal = [
+    { 
+      id: 'desarrollo-web',
+      title: 'Desarrollo Web',
+      li1: 'Diseño personalizado',
+      li2: 'Integracion de redes sociales',
+      li3: 'Optimización de Velocidad',
+      li4: 'Compatibilidad Multilingüe',
+      li5: 'Branding y Posicionamiento'
+    },
+    {
+      id: 'diseño-ui-ux',
+      title: 'Diseño UI-UX',
+      li1: 'Experiencia de usuario intuitiva',
+      li2: 'Diseño Responsivo',
+      li3: 'Estilo minimalista',
+      li4: 'componentes interactivo para el usuario',
+      li5: 'Accesibilidad Web'
+    },
+    {
+      id: 'desarrollo-eccommerse',
+      title: 'Desarrollo E-ccommerce',
+      li1: 'Experiencia de Compra Intuitiva',
+      li2: 'Personalización de Productos',
+      li3: 'Seguridad y Confianza',
+      li4: 'Mantenimiento Continuo',
+      li5: 'Escalabilidad'
+    },
+  ]
 
-    cardServices.classList.add('disabled-hover');
-    body.style.overflow = 'hidden'
-    navBar.style.display = 'none'
-    modal.style.display = 'block'
+  function openModal(modalId){
+    const modalData = servicesModal.find(modal => modal.id === modalId)
+
+    if(modalData){
+      const body = document.body
+      const modal = document.querySelector(`.modal.${modalId}`);
+      const cardsToStopTransform = document.querySelectorAll('.card')
+      const navBar = document.querySelector('#desktop-nav')
+      const modalTitle = modal.querySelector('.modal-title');
+      const modalContent1 = modal.querySelector('.modal-content-1');
+      const modalContent2 = modal.querySelector('.modal-content-2');
+      const modalContent3 = modal.querySelector('.modal-content-3');
+      const modalContent4 = modal.querySelector('.modal-content-4');
+      const modalContent5 = modal.querySelector('.modal-content-5');
+
+      cardsToStopTransform.forEach(card => {
+        card.classList.add('disabled-hover');
+      });
+      
+      modalTitle.textContent = modalData.title;
+      modalContent1.textContent = modalData.li1;
+      modalContent2.textContent = modalData.li2;
+      modalContent3.textContent = modalData.li3;
+      modalContent4.textContent = modalData.li4;
+      modalContent5.textContent = modalData.li5;
+
+      body.style.overflow = 'hidden'
+      modal.style.display = 'block'
+      navBar.style.display = 'none'
+    }
   }
 
-  function closeModale(){
+  function closeModal(){
+    const modal = document.querySelectorAll('.modal')
+    const cardsToStopTransform = document.querySelectorAll('.card')
+    const navBar = document.querySelector('#desktop-nav')
     const body = document.body
-    const navBar = document.querySelector('#desktop-nav');
-    const cardServices = document.querySelector('.card');
-    const modal = document.querySelector('.modal');
 
-    cardServices.classList.remove('disabled-hover')
-    body.style.overflow = ''
+    modal.forEach(element => {
+      element.style.display = 'none'
+    })
+
+    cardsToStopTransform.forEach(element => {
+      element.classList.remove('disabled-hover')
+    })
+
     navBar.style.display = 'flex'
-    modal.style.display = 'none'
+    body.style.overflow = ''
   }
