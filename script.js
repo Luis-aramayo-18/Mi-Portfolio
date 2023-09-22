a628dd17b1c11c9a14efe68a5fa6e6b382038516
Luis Aramayo <luis.aramayo.18@gmail.com>
2023-09-21 21:41:16 -0300

    corrijo menu hamburguesa

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// function toggleMenu(){
-//     const menu = document.querySelector('.menu-links');
-//     const icon = document.querySelector('.hamburguer-icon');
-
-//     menu.classList.toggle('open')
-//     icon.classList.toggle('open')
-// }
 
 function descargarCV() {
   const url = '../assets/curriculumVitaeMe.pdf';
@@ -282,4 +275,40 @@ const darkTheme = document.querySelector('.dark-mode')
 
 darkTheme.onclick = function (){
   document.body.classList.toggle('dark-theme');
+}
+
+const hamburguerMenu = document.querySelector('.hamburguer-icon')
+
+hamburguerMenu.onclick = function(){
+  const navBar = document.querySelector('#desktop-nav')
+  const header = document.querySelector('.name-header')
+  const darkTheme = document.querySelector('.dark-mode')
+  const menuLinks = document.querySelector('.container-links')
+  const navLinks = document.querySelector('.nav-links')
+  const close = document.querySelector('.close-hamburguer-menu')
+
+
+  menuLinks.style.display = 'block'
+  close.style.display = 'block'
+  hamburguerMenu.style.display = 'none'
+  header.style.display = 'none'
+  darkTheme.style.display = 'none'
+  navLinks.classList.add('links-modal')
+}
+
+const closeHamburguerMenu = document.querySelector('.close-hamburguer-menu')
+
+closeHamburguerMenu.onclick = function(){
+  const navBar = document.querySelector('#desktop-nav')
+  const header = document.querySelector('.name-header')
+  const darkTheme = document.querySelector('.dark-mode')
+  const menuLinks = document.querySelector('.container-links')
+  const navLinks = document.querySelector('.nav-links')
+
+  menuLinks.style.display = 'none'
+  closeHamburguerMenu.style.display = 'none'
+  hamburguerMenu.style.display = 'block'
+  header.style.display = 'block'
+  darkTheme.style.display = 'block'
+  navLinks.classList.remove('links-modal')
 }
